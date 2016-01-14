b7c96576cf0f46a105622b8523d6ebcbe09054f1
Viktor Shamal <sphinxominator@gmail.com>
2016-01-14 17:10:02 +0000

    added logo url to project json response

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,9 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :teaser, :description, :category, :homepage, :plans, :cover_image_url
-
-  def cover_image
-    object.cover_image.url
-  end
+  attributes :id, :title, :teaser, :description, :category, :homepage, :plans, :cover_image_url, :logo_url
 
   has_many :creatives, serializer: CreativeSerializer
   has_many :subscribers, serializer: SubscriberSerializer
