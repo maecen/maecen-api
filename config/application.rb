2b115d7612c8b48524b5aa87a9fd72041d26cc54
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2016-01-14 17:22:30 +0100

    filtered out base64 images from logs

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Maecen
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.filter_parameters << :password, :cover_image, :logo
+
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
