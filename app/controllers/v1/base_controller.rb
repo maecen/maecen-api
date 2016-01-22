69e1af194279706108d3b3d634bc9733707c84fa
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2016-01-22 15:50:34 +0100

    Switched default locale to danish and added some translations

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -1,4 +1,10 @@
 class V1::BaseController < ApplicationController
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
   resource_description do
     #api_version '1.0'
   end
